5af8e4fdaa5ed2ebc964d7ba81ea707c42810900
Timur Zhumaniezov <zovmur@gmail.com>
2026-05-06 22:19:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@
 // ============================================
 
 window.TSN_CONFIG = {
-  SUPABASE_URL: 'YOUR_SUPABASE_URL_HERE',
-  SUPABASE_ANON_KEY: 'YOUR_SUPABASE_ANON_KEY_HERE',
+  SUPABASE_URL: 'https://dmrivztnebgkqtemvlki.supabase.co/rest/v1/',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtcml2enRuZWJna3F0ZW12bGtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNzg1NjgsImV4cCI6MjA5MzY1NDU2OH0._jxh0tk-8K1ufTLXcygiezG02AUUn7aKM2DPlytQWxw',
 
   // Если оставить пустыми — сайт будет работать
   // в демо-режиме без отправки заявок и загрузки новостей
